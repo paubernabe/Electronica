4d05ed37157bd3fa4eb9bd781ae78004d0953af0
paubernabe <pbernaco7@alumnes.ub.edu>
2019-09-21 20:20:58 +0200

    p0

---
+++ p0_GRUP10/BlinkWithoutDelay/BlinkWithoutDelay.ino
@@ -0,0 +1,67 @@
+/* Blink without Delay
+ 
+ Turns on and off a light emitting diode(LED) connected to a digital  
+ pin, without using the delay() function.  This means that other code
+ can run at the same time without being interrupted by the LED code.
+ 
+ The circuit:
+ * LED attached from pin 13 to ground.
+ * Note: on most Arduinos, there is already an LED on the board
+ that's attached to pin 13, so no hardware is needed for this example.
+ 
+ 
+ created 2005
+ by David A. Mellis
+ modified 8 Feb 2010
+ by Paul Stoffregen
+ modified 27 Apr 2012
+ by Robert Wessels
+ 
+ This example code is in the public domain.
+
+ 
+ http://www.arduino.cc/en/Tutorial/BlinkWithoutDelay
+ */
+
+//const int ledPin =  GREEN_LED;      // the number of the LED pin
+const int ledPin =  78; //CANVIEM LA CONSTANT GREEN_LED PER EL NUM 78 QUE REPRESENTA EL PIN DEL LED VERMELL
+
+int ledState = LOW;             // L'ESTAT INICIAL DEL LED SERÀ APAGAT(LOW = 0 V)
+long previousMillis = 0;        // UTILITZAREM AQUEST COM A BASE DEL COMPTADOR DE TEMPS PER MILISEGONS
+int control = 0;
+
+long interval2 = 1;
+long interval = 10;           // DEFINIM EL TEMPS ENTRE QUE S'ENCÉN I S'APAGA
+
+void setup() { //FUNCIÓ QUE UTILITZAREM PER LA CONFIGURACIÓ DELS PINS
+  pinMode(ledPin, OUTPUT);  //EL PIN DEL LED EL DEFINIM COM A SORTIDA    
+}
+
+void loop() //ES CRIDA RECURSIVAMENT
+{
+  unsigned long currentMillis = millis(); //AQUESTA VARIABLE +=1 CADA MILISEGON
+ 
+  if(currentMillis - previousMillis > interval) { //AQUI ENTRA QUAN SIGUI D'APAGAT A ENCÉS
+    //SI HA PASSAT MÉS TEMPS DEL QUE HEM DEFINIT 
+    previousMillis = currentMillis;   
+
+    //CADA COP QUE HAGI PASSAT L'INTERVAL ES POSARÀ A L'ESTAT CONTRARI
+    if (ledState == LOW)
+      ledState = HIGH;
+    else
+      ledState = LOW;
+
+    digitalWrite(ledPin, ledState); //S'ECRIU AL PIN L'ESTAT AL QUE ESTÀ(ENCÉN EL PIN)
+
+     control=0; //ENS INDICARÀ QUE EL LED ESTA ENCÉS
+     
+ }
+  //
+    else {
+      if ((currentMillis - previousMillis > interval2) && (control==0)) {  //AQUI ES COMPLIRÀ EN EL TEMPS D'ENCÉS A APAGAT
+        digitalWrite(ledPin, LOW); //S'ECRIU AL PIN L'ESTAT AL QUE ESTÀ(APAGA EL PIN)
+        control=1; //ENS MARCARÀ QUE EL LED ESTÀ APAGAT
+      }
+    }
+}
+//EN CONCLUSIÓ EN CADA MÉS TEMPS ESTÀ APAGAT SIGNIFICARÀ UNA LLUM MENYS LLUMINOSA
---
+++ p0_GRUP10/Button/Button.ino
@@ -0,0 +1,76 @@
+/*
+  Button
+ 
+ Turns on and off a light emitting diode(LED) connected to digital  
+ pin 13, when pressing a pushbutton attached to pin 2. 
+ 
+ 
+ The circuit:
+ * LED attached from pin 13 to ground 
+ * pushbutton attached to pin 2 from +3.3V
+ * 10K resistor attached to pin 2 from ground
+ 
+ * Note: on most Arduinos there is already an LED on the board
+ attached to pin 13.
+ 
+ 
+ created 2005
+ by DojoDave <http://www.0j0.org>
+ modified 30 Aug 2011
+ by Tom Igoe
+ modified Apr 27 2012
+ by Robert Wessels
+ 
+ This example code is in the public domain.
+ 
+ http://www.arduino.cc/en/Tutorial/Button
+ */
+
+// constants won't change. They're used here to 
+// set pin numbers:
+const int buttonPin = PUSH2;     //CONSTANT DEL BOTÓ QUE POLSAREM
+const int ledPinG =  GREEN_LED;      // CONSTANTS DEL LEDS QUE UTILITZAREM
+const int ledPinB =  BLUE_LED; 
+const int ledPinR =  RED_LED; 
+const int buttonPinBis = PUSH1; //CONSTANT DE L'ALTRE BOTÓ QUE PODREM POLSAR
+
+long interval = 100;      //INTERVAL DE 100ms
+long previousmillis = 0;
+int buttonState = 0;         // VARIABLE D'ESTAT DEL BOTÓ INICIALMENT A 0
+int buttonStateBis = 0;     //VARIABLE D'ESTAT DE L'ALTRE BOTÓ
+
+void setup() {
+  pinMode(ledPinG, OUTPUT);      //POSAREM ELS LEDS COM A SORTIDA
+  pinMode(ledPinB, OUTPUT);
+  pinMode(ledPinR, OUTPUT);
+  pinMode(buttonPinBis, INPUT_PULLUP);
+  pinMode(buttonPin, INPUT_PULLUP);     //POSAREM EL BOTÓ COM A ENTRADA
+}
+
+void loop(){
+
+
+  unsigned long currentMillis = millis();
+
+  if(currentMillis - previousmillis > interval) { //CADA 100ms LLEGIREM L'ESTAT DELS BOTONS
+    buttonState = digitalRead(buttonPin); //LLEGIREM L'ESTAT DELS BOTONS
+    buttonStateBis =  digitalRead(buttonPinBis);
+    previousmillis = currentMillis;
+  }
+
+
+  if ((buttonState == HIGH && buttonStateBis == LOW) || (buttonState == LOW && buttonStateBis == HIGH))  {  // SI APRETEM UN BOTÓ O L'ALTRE S'APAGARÀ EL LED VERD DEL RGB 
+    digitalWrite(ledPinG, LOW);  
+  }
+  else if(buttonState == LOW && buttonStateBis == LOW){ //SI APRETEM ELS DOS BOTONS, S'ENCENDRAN ELS 3 LEDS DEL CONJUNT RGB
+     digitalWrite(ledPinR, HIGH);
+     digitalWrite(ledPinG, HIGH);
+     digitalWrite(ledPinB, HIGH);
+     
+  }
+  else {                  //ALTRAMENT EL LED VERD SERÀ L'ÚNIC QUE CONTINUARÀ ENCÈS
+    digitalWrite(ledPinG, HIGH); 
+    digitalWrite(ledPinR, LOW); 
+    digitalWrite(ledPinB, LOW); 
+  }
+}
