98ef9c88051a24239b63461db7c4da7273429792
paubernabe <pbernaco7@alumnes.ub.edu>
2019-10-24 10:16:55 +0200

    p2 beta bis

--- PauBernabeNilVinals_p2/PauBernabeNilVinals_p2.ino
+++ PauBernabeNilVinals_p2/PauBernabeNilVinals_p2.ino
@@ -6,7 +6,9 @@
  */
 unsigned char pinlect=2;
 unsigned char pindig=31;
-int valueV0;
+float valueV0;
+long interval = 5000;      //INTERVAL DE 5s
+long previousmillis = 0;
 void setup() {
   Serial.begin(115200);
   pinMode(pindig, INPUT_PULLUP);
@@ -14,13 +16,15 @@ void setup() {
 }
 
 void loop() {
-  unsigned int control=0;
+  unsigned int control;
   //Definim el nostre rang entre 0.5V i 2.0V.
   float vo_min = 0.5;
   float vo_max = 2.0;
+  
+  unsigned long currentMillis = millis();
 
-  valueV0=analogRead(pinlect); //amb això sabem el valor de v0
-  control=digitalRead(pindig); //amb això sabem si accionem el l'interruptor o no
+  valueV0=analogRead(pinlect); //amb aquesta variable sabem el valor de v0
+  control=digitalRead(pindig); //amb aquesta variable sabem si accionem el l'interruptor o no
 
   //Plotejarem vo_min i vo_max tota l'estona.
   Serial.println(vo_min);
@@ -28,10 +32,15 @@ void loop() {
 
   //Si accionem l'interruptor deixarem de mostrar v0 durant un temps
   if (control == LOW){
-    delay(5000);
-    control == HIGH;
+    if (currentMillis - previousmillis > interval){
+      //Quan ja hagi passat l'interval posem el control a high
+      previousmillis = currentMillis;
+      digitalWrite(control, HIGH);
+    }
+    
+  }else{
+    Serial.println(valueV0);
   }
-  Serial.println(valueV0);
   delay(100); //Fem la mesura cada 100ms com es demana
   
 
